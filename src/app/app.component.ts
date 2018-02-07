e5f4993681ab4cc8b79a971d447cf030f74110e4
Esteban Santamaria <esteban.santamaria@yuxpacific.com>
2018-02-07 11:52:48 -0500

    upload exercise 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  title = 'app';
+	title = 'app';
+	imageUrl: string;
+	baseUrl = 'https://api.adorable.io/avatars/500/';
+	urlSuffix = '@adorable.io.png';
+	idenfier: number;
+
+	constructor() {
+		setInterval(() => {
+			this.idenfier = Math.floor(Math.random() * (10000000000000 + 1));
+		}, 1000);
+	}
+	generateRandomFace() {
+		this.imageUrl = `${this.baseUrl}${this.idenfier}${this.urlSuffix}`;
+		return this.imageUrl;
+	}
 }
