a57241c8dae14b57539ee7e0f567fc0121e3da4a
Esteban Santamaria <esteban.santamaria@yuxpacific.com>
2018-02-07 12:00:56 -0500

    improve exercise 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,16 @@ export class AppComponent {
 	imageUrl: string;
 	baseUrl = 'https://api.adorable.io/avatars/500/';
 	urlSuffix = '@adorable.io.png';
-	idenfier: number;
 
 	constructor() {
-		setInterval(() => {
-			this.idenfier = Math.floor(Math.random() * (10000000000000 + 1));
-		}, 1000);
+		setInterval(() => this.generateRandomFace(), 1000);
 	}
+
 	generateRandomFace() {
-		this.imageUrl = `${this.baseUrl}${this.idenfier}${this.urlSuffix}`;
-		return this.imageUrl;
+		this.imageUrl = `${this.baseUrl}${this.generateRandomWord()}${this.urlSuffix}`;
+	}
+
+	generateRandomWord(): string {
+		return `${Math.floor(Math.random() * (10000000000000 + 1))}`;
 	}
 }
